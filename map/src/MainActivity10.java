ee97ef5754b89793107d808d926e967da56a93fe
takagotch <dyaccb@gmail.com>
2018-03-29 15:09:54 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- map/src/MainActivity10.java
+++ map/src/MainActivity10.java
@@ -1,3 +1,17 @@
+//xmlns:android
+//class
+//map:cameraBearing
+//map:cameraTargetLat
+//map:cameraTilt
+//map:cameraZoom
+//map:cameraType
+//map:uiCompas
+//map:uiRotateGestures
+//map:uiScrollGestures
+//map:uiTiltGestures
+//map:uiZoomcontrols
+//map:uiZoomGestures
+
 public class MainActivity extends FragmentActivity{
   @Override
   protected void onCreate(Bundle savedInstanceState){
