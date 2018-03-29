9452a36fb6b889f0413ebdd06186540a03e75b9f
takagotch <dyaccb@gmail.com>
2018-03-29 15:13:34 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ map/cmd.sh
@@ -0,0 +1,12 @@
+//<meta-data
+//	android:name="com.google.android.maps.v2.API_KEY"
+//	android:value="%API KEY%" />
+//
+//<permission
+//      android:name="*.permission.MAPS_RECEIVE"
+//      android:protectionLevel="signature" />
+//<uses-permission android:name="*.permission.MAPS_RECEIVE" />
+//
+//
+
+keytoool -list -keystore debug.keystore
