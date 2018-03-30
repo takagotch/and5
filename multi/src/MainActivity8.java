a358e3f84689b9d181b618cb57ed7396e8cb33ed
takagotch <dyaccb@gmail.com>
2018-03-30 15:30:16 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- multi/src/MainActivity8.java
+++ multi/src/MainActivity8.java
@@ -51,13 +51,31 @@ protected void onResume(){
   arraySound = new ArrayList<Integer>();
   mPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
   arraySound.add(0, mPool.load(this, R.raw.cat, 1));
-  arraySound.add(1, mPool.load(), 1);
-  arraySound.add();
-  arraySound.add();
-  arraySound.add();
-  mPool.setOnLoadCompletelistener(new SoundPool, int sampleId, int status){
+  arraySound.add(1, mPool.load(this, R.raw.crows, 1));
+  arraySound.add(2, mPool.load(this, R.raw.door_chime, 1));
+  arraySound.add(3, mPool.load(this, R.raw.knocking_iron_door1, 1));
+  arraySound.add(4, mPool.load(this, R.raw.station_announce, 1));
+  mPool.setOnLoadCompletelistener(new SoundPool, int sampleId, int status(){
     @Override
-    public void onLoadComplete(){}
+    public void onLoadComplete(SoundPool soundPool, int sampleId, int status){
+      if(status == 0){
+        Toast.makeText(getApplicationContext(), sampleId + "COMPLETE", Toast.LENGTH_SHORT).show();
+      }
+    }
+  });
+}
+
+public void onSoundPool(View v){
+  Random random = new Random();
+  int r = random.nextInt(5);
+  mPool.play(arraySound.get(r), 1.0f, 1.0f, 1, 0, 1.0f);
+}
+
+@Override
+protected void onPause(){
+  if(mPool != null){
+    mPool.release();
   }
+  super.onPause();
 }
 
