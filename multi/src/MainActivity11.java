cde8d2ae65018b6c41e8519fe0ca76441958d48a
takagotch <dyaccb@gmail.com>
2018-03-30 16:30:53 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- multi/src/MainActivity11.java
+++ multi/src/MainActivity11.java
@@ -32,34 +32,93 @@ public class MainActivity extends Activity {
 
     mRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
 
-    mRecorder.setOutputFormat();
+    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 
-    mRecorder.setVieo();
+    mRecorder.setVieo(MediaRecorder.VideoEncorder.MPEG_4_SP);
 
-    String path = Environment.getExternalStorageDirectory() + "";
-    mRecorder.setOutputFile();
+    String path = Environment.getExternalStorageDirectory() + "/video_record.3gp";
+    mRecorder.setOutputFile(path);
 
-    mRecorder.setVideoFrameRate();
-    mRecorder.setVideoSize();
+    mRecorder.setVideoFrameRate(30);
+    mRecorder.setVideoSize(640, 400);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu){
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
   }
 
   @Override
   public void onResume(){
+    super.onResume();
   };
 
   @Override
   public void onPause(){
+    super.onResume();
   };
 
   CopoundButton.OnCheckChangeListener mListener = new CompondButton.OnCheckedChangeListener(){
+    @Override
+    public void onCheckChanged(CompoundButton buttonView, boolean isChecked){
+      if(isChecked){
+        try{
+	  mRecorder.prepare();
+	} catch(IllegalStateException e){
+	  e.printStackTrace();
+	} catch(IOException e){
+	  e.printStackTrace();
+	}
+
+	mRecorder.start();
+      } else {
+        if(Recorder != null){
+	  mRecorder.stop();
+	  mRecorder.reset();
+	}
+      }
+    }
   };
 
-  private class PreviewView extends SurfaceView {
-  
+  private class PreviewView extends SurfaceView
+		  implements SurfaceHolder.Callback{
+    private SurfaceHolder mHolder = null;
+    public PreviewView(Context context){
+      super(context);
+
+      mHolder = getHolder();
+      mHolder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreate(SurfaceHolder holder){
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+      if(mRecorder != null){
+        mRecorder.setPreviewDisplay(mHolder.getSurface());
+
+	try{
+	  mRecorder.prepare();
+	} catch(IllegalStateException e){
+	  e.printStackTrace();
+	} catch(){
+	  e.printStackTrace();
+	}
+
+	mRecorder.start();
+      }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder){
+      mRecorder.stop();
+      mRecorder.reset();
+      mRecorder.release();
+      mRecorder = null;
+    }
   }
 }
 
