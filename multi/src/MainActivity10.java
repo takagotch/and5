4f71ff4d34a5e5988d661a04409ecf7e6daebf4c
takagotch <dyaccb@gmail.com>
2018-03-30 16:12:11 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ multi/src/MainActivity10.java
@@ -0,0 +1,12 @@
+//void setVieoPath(String path)
+//void start()
+//boolean isPlaying()
+//
+//media_VideoView
+
+VideoView vv = (VideoView) findViewById(R.id.videoView);
+
+vv.setVideoPath("/mnt/sdcard/Movies/movie.mp4");
+
+vv.start();
+
