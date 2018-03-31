96b5d6575927fc1e54ac43ca667d90a92ecfffc6
takagotch <dyaccb@gmail.com>
2018-04-01 08:47:17 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- linked/src/MainActivity8.java
+++ linked/src/MainActivity8.java
@@ -1,5 +1,17 @@
-//Uri parse()
-//
+//Uri parse(String uriString)
+//Intent()
+//Intent(String action)
+//Intent(String  action, Uri uri)
+//Intent setAction(String action)
+//Intent addCategory(String category)
 
-
+Button btn_browser = (Button) findViewById(R.id.btn_browser);
+btn_browser.setOnClickListener(new View.OnClickListener(){
+  @Override
+  public void onClick(View v){
+    Uri uri = Uri.parse("http://www.gihyo.co.jp/");
+    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+    startActivity(intent);
+  }
+});
 
