d6f448e9512331a2e2cd60576e4b2a91daeb89a0
takagotch <dyaccb@gmail.com>
2018-03-31 22:52:38 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- linked/src/MainActivity7.java
+++ linked/src/MainActivity7.java
@@ -1,20 +1,29 @@
+//void set(int type, long triggerAtTime, PendingIntent operation)
+//void setRepeat(int type, long triggerAtMillis, ong intervalMillis, PendingIntent operation)
+//void cancel(PendingIntent operation)
+//void setWindow(int type, long windowStartMillis, long windowLengthMillis, PendingIntent operation)
+//void setExact(int type, long triggerAtMillis, PendingIntent operation)
 //
+//ELAPSED_REALTIME
+//ELAPSED_REALTIME_WAKEUP
+//RTC
+//RTC_WAKEUP
 
 @Override
 protected void onCreate(Bundle savedInstanceState){
   super.onCreate(savedInstanceState);
   setContentView(R.layout.activity_main);
 
-  Intent intent = new Intent();
-  intent.setFlags();
-  PendingIntent sender = PendingIntent.getBroadcast();
+  Intent intent = new Intent(getApplicationContext(), AlarmReceiver.class);
+  intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+  PendingIntent sender = PendingIntent.getBroadcast(thid, 0, intent, 0);
 
   Calendar calendar = Calendar.getInstance();
   calendar.setTimeInMillis(System.currentTimeMillis());
   calendar.add(Calendar.SECOND, 10);
 
-  AlarmManager alarmManager = () getSystemService();
-  alarmManager.set();
+  AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+  alarmManager.set(AlarmManager.RTC_WAKEUP, calendarTimeMillis(), sender);
 }
 
 @Override
