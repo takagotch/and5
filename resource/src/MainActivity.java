6d55191f0639bc57f7efd75da662b1691776a013
takagotch <dyaccb@gmail.com>
2018-04-02 00:50:47 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ resource/src/MainActivity.java
@@ -0,0 +1,20 @@
+//Resource getResource()
+
+Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.photo2);
+
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+  <string name="app_name">ui_TextView</string>
+  <string name="action_settings">Settings</string>
+  <string name="hello">hello</string>
+
+  <string name="action_replace">Text</string>
+
+
+  <color name="color_main_background"></color>
+
+</resources>
+
+
+
