8e9b2ab21a2a20969036fcc9c24c12539c4a78df
takagotch <dyaccb@gmail.com>
2018-04-03 02:14:22 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- all/src/MainActivity3.java
+++ all/src/MainActivity3.java
@@ -31,7 +31,29 @@ if(actionBar.isShowing()){
 
 @Override
 protected void onCreate(Bundle savedInstanceState){
-
+  ActionBar actionBar = getSupportActionBar();
+  actionBar.setDisplayHomeAsUpEnabled(true);
 }
 
+@Override
+public boolean onOptionsItemSelected(MenuItem item){
+  switch(item.getItemId()){
+	  case android.R.id.home:
+		  Toast.makeText(this, "SELECT", Toast.LENGTH_SHORT).show();
+		  break;
+	  default:
+		  break;
+  }
+  return super.onOptionsItemSelected(item);
+}
+
+//void setShowAsAction(MenuItem item, int actionEnum)
+//SHOW_AS_ACTION_ALWAYS
+//SHOW_AS_ACTION_IF_ROOM
+//SHOW_AS_ACTION_NAVER
+
+@Override
+public boolean onCreateOptionMenu(Menu menu){
+  getMenuInflater().inflate();
+}
 
