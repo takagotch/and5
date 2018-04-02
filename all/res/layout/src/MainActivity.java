6705ee9994acf7367bd54b14a23d83051e62dc83
takagotch <dyaccb@gmail.com>
2018-04-03 01:37:34 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ all/res/layout/src/MainActivity.java
@@ -0,0 +1,8 @@
+public class MainActivity extends FragmentActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
+
---
+++ all/src/TopFragment.java
@@ -0,0 +1,8 @@
+public class TopFragment extends Fragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		  Bundle savedInstanceState){
+    return inflater.inflate(R.layout.fragment_top, container);
+  }
+}
+
