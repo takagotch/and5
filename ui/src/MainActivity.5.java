db6e12aad2b3961d2549c83ffd5cf43591f7890b
takagotch <dyaccb@gmail.com>
2018-04-03 12:07:33 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- ui/src/MainActivity.5.java
+++ ui/src/MainActivity.5.java
@@ -29,6 +29,76 @@ TextView tvHello = (TextView) findViewById(R.id.tvHello);
 
 tvHello.setTypeface(tfOrigin);
 
+//Toast makeText(Context context, CharSequence text, int duration)
+//Toast makeText(Context context, int resId, int duration)
+//void setGravity(int gravity, int xOffset, int yOffset)
+//void show()
+//LENGTH_SHORT
+//LENGTH_LONG
+
+Button btnToast = (Button) findViewById(R.id.btnToast);
+btnToast.setOnClickListener(new View.OnClickListener(){
+  @Override
+  public void onClick(View v){
+    Toast.makeText(
+		    getApplicationContext(),
+		    "CLICK BUTTON",
+		    Toast.LENGTH_LONG).show();
+  }
+});
+
+//void setView(View view)
+//LayoutInflater getLayoutInflater()
+//View inflate(int resource, ViewGroup root)
+Button btnToast = (Button)findViewById(R.id.btnToastOrigin);
+btnToast.setOnClickListener(new View.OnClickListener(){
+  @Override
+  public void onClick(View v){
+    LayoutInflater layoutInflater = getLayoutInflater();
+    View customView = layoutInflater.inflate(
+	R.layout.toast_origin, (ViewGroup) findViewById(R.id.root_ToastOrigin));
+    Toast orgToast = Toast.makeText(v.getContext(), "", Toast.LENGTH_LONG);
+    orgToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+    orgToast.setView(customView);
+    orgToast.show();
+  }
+});
+
+
+
+public void onClickEvent(View v){
+  Toast.makeText(this,
+		  "BUTTON CLICK",
+		  Toast.LENGTH_LONG).show();
+}
+
+
+//void setOnCheckChangeListener(OnCheckedChangeListener listener)
+//void onCheckedChanged(RadioGroup group, int checkedId)
+RadioGroup grpColor = (RadioGroup) findViewById(R.di.grp_color);
+grpColor.setOnCheckedChanged(RadioGroup group, int checkedId){
+  public void onChecked(RadioGroup group, int checkedId){
+    LinearLayout layout =
+	    (LinearLayout) findViewById(R.id.layout_root);
+
+    switch (checkedId){
+	    case R.id.rbtn_white:
+		    layout.setBackgroundColor(Color.WHITE);
+		    break;
+	    case R.id.rbtn_blue:
+		    layout.setBackgroundColor(Color.BLUE);
+		    break;
+	    case R.id.rbtn_yellow:
+		    layout.setBackgroundColor(Color.YELLOW);
+		    break;
+
+    }
+  }
+};
+
+
+//void setTextOn()
+//void setTextOff()
 
 
 
