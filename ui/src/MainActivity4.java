9db886c36fcb21493f7da103ad62f431942dfbfa
takagotch <dyaccb@gmail.com>
2018-04-03 10:45:57 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ ui/src/MainActivity4.java
@@ -0,0 +1,20 @@
+//View findViewById(int id)
+
+TextView tvPokeri = (TextView) findViewById(R.id.tvPokeri);
+
+//android:layout_width
+//android:layout_height
+//wrap_content
+//fill_parent
+//match_parent
+/*
+<LinearLayout
+  xmlns:android="http://schemas.android.com/apk/res/android"
+  android:layout_width="match_parent"
+  android:layout_height="match_parent"
+  android:orientation="vertical" >
+*/
+
+
+
+
